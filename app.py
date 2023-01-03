6481375b550822841489153c2038227d27829d45
DayoTitiloye-code <DayoTitiloye@outlook.com>
2023-01-03 16:25:22 +0000

    added error handling routes

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from flask_cors import CORS
+from werkzeug import exceptions
 
 app = Flask(__name__)
 
@@ -12,6 +13,16 @@ def home():
 @app.route('/about')
 def about_page():
     return render_template('about.html'), 200
+
+
+@app.errorhandler(exceptions.NotFound)
+def handle_404(err):
+    return jsonify({"message": f"Sorry... {err}"}), 404
+
+
+@app.errorhandler(exceptions.InternalServerError)
+def handle_500(err):
+    return jsonify({"message": f"It's not you, it's us"}), 500
 
 if __name__ == '__main__':
     app.run()

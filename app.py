5c2e713e4f533ce4905d75b05087c4a8586949a7
DayoTitiloye-code <DayoTitiloye@outlook.com>
2023-01-03 14:36:10 +0000

    initialized home route

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 CORS(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html'), 200
+
+if __name__ == '__main__':
+    app.run()

f8074ea32518a3521f11eeaf700d6fc84f9394fe
DayoTitiloye-code <DayoTitiloye@outlook.com>
2023-01-19 10:04:46 +0000

    finished contact form, fixed routes

--- app.py
+++ app.py
@@ -6,30 +6,52 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.sqlite3'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
 CORS(app)
+
 db = SQLAlchemy(app)
 
 class People(db.Model):
     _id = db.Column('id', db.Integer, primary_key = True)
-    firstName = db.Column('first name', db.String(30), nullable = False)
-    lastName = db.Column('last name', db.String(40), nullable = False)
-    email = db.Column('email', db.String(60), nullable = False)
-    business = db.Column('business/company', db.String(50), nullable = False)
-    reason = db.Column('contact reason', db.Text, nullable = False)
+    firstName = db.Column('first name', db.String(30))
+    lastName = db.Column('last name', db.String(40))
+    email = db.Column('email', db.String(100))
+    business = db.Column('business', db.String(50))
+    reason = db.Column('reason for contact', db.Text)
+
+    def __init__(self, firstName, lastName, email, business, reason):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.business = business
+        self.reason = reason
 
 @app.route('/')
 def home():
     return render_template('index.html'), 200
 
-@app.route('/people', methods = ['GET'])
+@app.route('/people', methods = ['POST','GET'])
 def contact_form():
-    firstName = request.args.get('firstName')
-    lastName = request.args.get('lastName')
-    email = request.args.get('email')
-    business = request.args.get('business')
-    reason = request.args.get('reason')
-    return render_template('index.html'), 200
+    firstName = None
+    lastName = None
+    email = None
+    business = None
+    reason = None
+    if request.method == 'POST':
+        firstName = request.form['firstName']
+        lastName = request.form['lastName']
+        email = request.form['email']
+        business = request.form['business']
+        reason = request.form['reason']
+        form_data = request.form
+        person = People(firstName, lastName, email, business, reason)
+        db.session.add(person)
+        db.session.commit()
+        # return render_template('people.html', form_data = form_data), 200
+    return render_template('index.html', form_data=form_data, firstName=firstName, lastName=lastName, email=email, business=business, reason=reason), 200
+
+@app.route('/view')
+def people():
+    return render_template('people.html', values = People.query.all())
 
 @app.route('/about')
 def about_page():
